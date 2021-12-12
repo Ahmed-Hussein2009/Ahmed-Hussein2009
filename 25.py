9575f4011e97e2b8afb77064de2397485babc7ea
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-12 13:04:23 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Skin Cancer Classfication ")
 file = st.file_uploader("Pick a file")
-pet = st.radio("Pick a pet", pets)
+
 date = st.date_input("Pick a date")
 number = st.slider("Pick a number", 0, 100)
