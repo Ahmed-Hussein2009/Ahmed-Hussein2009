4001741203d56ca031ab7d9a98efee78258f0204
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-12 13:58:25 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -2,6 +2,8 @@ import streamlit as st
 st.title("Skin Cancer Classfication ")
 st.write("ID")
 your_name = st.text_input("Enter your name")
+col_one.header("Column 1")
+col_two.header("Column 2")
 file = st.file_uploader("Upload Cancer photo")
 
 date = st.date_input("Pick a date")
