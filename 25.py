10ea5937a422502ab7ab0508ab802041ac8bf15c
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-12 13:35:56 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title("Skin Cancer Classfication ")
 st.write("ID")
+st.text_input(label, value="", max_chars=None, key=None, type="default", help=None, autocomplete=None, on_change=None, args=None, kwargs=None, *, placeholder=None)
 file = st.file_uploader("Upload Cancer photo")
 
 date = st.date_input("Pick a date")
