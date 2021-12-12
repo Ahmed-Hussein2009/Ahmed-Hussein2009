07447ad32bf999bda7f7c2be1bb8dd517a55cea5
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-12 14:16:44 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,7 +1,11 @@
 import streamlit as st
 st.title("Skin Cancer Classfication ")
 st.write("ID")
-your_name = st.text_input("Enter your name")
+name = st.text_input("Enter your name")
+st.write("Name")
+name = st.text_input("Enter your name")
+st.write("age")
+name = st.text_input("Enter your name")
 option = st.selectbox(
     "Select Gender",
     ["Male","Female" ]
@@ -11,5 +15,4 @@ file = st.file_uploader("Upload Cancer photo")
 # date = st.date_input("Pick a date")
 if st.button('Say hello'):
      st.write('Why hello there')
- else:
-     st.write('Goodbye')
+
