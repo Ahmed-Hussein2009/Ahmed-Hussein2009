53e749b5d7fd74c10e7d6f9ab59e59384aa26d1e
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-12 18:48:23 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,25 +1,63 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.title("Skin Cancer Classfication ,"ASD")
+st.title("Skin Cancer Classfication )
 
-ID = st.text_input("ID")
-name = st.text_input("Name")
-age = st.text_input(" Age")
-option = st.selectbox(
+form = st.form(key='my_form')
+         
+ID = form.text_input("ID")
+name = form.text_input("Name")
+age = form.st.number_input(" Age")
+option = form.selectbox(
     "Select Gender",
     ["Male","Female" ]
 )
-file = st.file_uploader("Upload Cancer photo")
-st.write(file)
-# date = st.date_input("Pick a date")
-components.html("""
-<style>
-body {
-  background-image: url('img_girl.jpg');
-}
-</style>""")
-components.html("""<div ">asd</div>""")
-color = st.color_picker('Pick A Color', '#00f900')
-st.write('The current color is', color)
+file = form.file_uploader("Upload Cancer photo")
+
+# form.text_input(label='Enter some text')
+submit_button = form.form_submit_button(label='Submit')
+         
+ 
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+# st.write(file)
+# # date = st.date_input("Pick a date")
+# components.html("""
+# <style>
+# body {
+#   background-image: url('img_girl.jpg');
+# }
+# </style>""")
+# components.html("""<div ">asd</div>""")
+# color = st.color_picker('Pick A Color', '#00f900')
+# st.write('The current color is', color)
 
         
