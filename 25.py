cbbabbc431e772fb6efc1a18fe395bf0bd2603b3
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-12 18:43:33 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import streamlit.components.v1 as components
-st.title("Skin Cancer Classfication ")
+st.title("Skin Cancer Classfication ,"ASD")
 
 ID = st.text_input("ID")
 name = st.text_input("Name")
