be13bcc77c885e8c9046b5f7b2460de10e93e478
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-16 08:15:02 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -18,22 +18,6 @@ file = form.file_uploader("Upload Cancer photo")
 
 submit_button = form.form_submit_button(label='Submit')
 
-import SessionState
-button1 = st.empty()
-text1 = st.empty()
-button2 = st.empty()
-text2 = st.empty()
-
-ss = SessionState.get(button1 = False)
-
-if button1.button('1') or ss.button2:
-    ss.button1 = True
-
-if ss.button1:
-    text1.write('you clicked the first button')
-    if button2.button('2'):
-        text2.write('you clicked the second button')
-
          
  
          
