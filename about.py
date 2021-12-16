6cea6f515110ab3c76a056a55c943faa86a19605
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-16 10:27:26 +0200

    Update about.py

--- about.py
+++ about.py
@@ -3,7 +3,12 @@ import streamlit.components.v1 as components
 st.title("About us")
 st.image("about.png")
 st.image("footer.png",width=700)   
-      
+<style>
+.stApp {
+  background-image: url("data:image/png;base64,%s");
+  background-size: cover;
+}
+</style>
 #width=650   
          
          
