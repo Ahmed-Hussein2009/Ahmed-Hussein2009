0253ff33ed8c4d873cbe611fcbfb0a5e4bc4632d
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-16 09:49:24 +0200

    Update 25.py

--- 25.py
+++ 25.py
@@ -21,11 +21,10 @@ submit_button = form.form_submit_button(label='Submit')
 
 
 placeholder = st.empty()
-Reset = placeholder.text_input('text')
-# click_clear = st.button('clear text input', key=1)
-if Reset:
+input = placeholder.text_input('text')
+click_clear = st.button('clear text input', key=1)
+if click_clear:
     input = form.text_input('text', value='', key=1)
-         
 
          
  
