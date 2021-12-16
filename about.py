db733553f35ab28734f4b2b2138b3c0e608d9890
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-16 10:31:54 +0200

    Update about.py

--- about.py
+++ about.py
@@ -3,8 +3,8 @@ import streamlit.components.v1 as components
 st.title("About us")
 st.image("about.png")
 st.image("footer.png",width=700)   
-def set_png_as_page_bg(1.png):
-    bin_str = get_base64_of_bin_file2(png_file) 
+def set_png_as_page_bg("1.png"):
+    bin_str = get_base64_of_bin_file2("1.png") 
     page_bg_img = '''
     <style>
     .stApp {
