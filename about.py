86ba5c34432073b0fa3ba33a2813aef3d82d01f1
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-16 10:17:37 +0200

    Update about.py

--- about.py
+++ about.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 st.title("About us")
 st.image("about.png")
-st.image("footer.png",width=720)   
+st.image("footer.png",width=700)   
       
 #width=650   
          
