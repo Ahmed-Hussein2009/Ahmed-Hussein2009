f0121a8927c72b19016d44cf2b46ba9f37659ff3
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 11:39:14 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Home")
-
+img1=st.image("Home1.png",width=1200)  
 start_color, end_color = st.select_slider(
      'Select a range of color wavelength',
      options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
