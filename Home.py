d429eff7fe204641599a4b6f9d9174898dc85189
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 11:37:10 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,8 +5,8 @@ start_color, end_color = st.select_slider(
      'Select a range of color wavelength',
      options=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'],
      value=('red', 'blue'))
-st.write('You selected wavelengths between', start_color, 'and', end_color)         
-         
+st.write('You selected wavelengths between', start_color,)         
+st.image("footer.png",width=1200)      
          
          
         
