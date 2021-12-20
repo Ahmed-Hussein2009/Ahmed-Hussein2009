fbc3e48e04bd97d6c2b11a47ef8290e250d71c6d
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 16:16:34 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,7 +5,7 @@ st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootst
 st.markdown("""
 <nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #000000;">
   <a class="navbar-brand" href="https://youtube.com/dataprofessor" target="_blank">Skin Cancer Classfication </a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="Ture" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div style="float:right" class="collapse navbar-collapse" id="navbarNav" >
