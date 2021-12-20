0ab860e693fd48962fc426abc2d714b24e4ba868
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 12:32:06 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -14,32 +14,48 @@ if st.button('Say hello'):
 st.image("footer.png",width=900)   
 """
 import streamlit as st
-import pickle as pkle
-import os.path
+import pandas as pd
+import numpy as np
 
-pages = ['Home','about','Page3']
+#add an import to Hydralit
+from hydralit import HydraHeadApp
 
-if os.path.isfile('next.p'):
-    next_clicked = pkle.load(open('next.p', 'rb'))
-    if next_clicked == len(pages):
-        next_clicked = 0 
-else:
-    next_clicked = 0 
+#create a wrapper class
+class MySampleApp(HydraHeadApp):
 
-if next:
-    next_clicked = next_clicked+1
-    if next_clicked == len(pages):
-        next_clicked = 0 
+#wrap all your code in this method and you should be done
+    def run(self):
+        #-------------------existing untouched code------------------------------------------
+        st.title('Uber pickups in NYC')
 
-choice = st.sidebar.radio("Pages",('Home','about', 'Page3'), index=next_clicked)
-pkle.dump(pages.index(choice), open('next.p', 'wb'))
+        DATE_COLUMN = 'date/time'
+        DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+                    'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-if choice == 'Page1':
-    st.title('Page 1')
-elif choice == 'Page2':
-    st.title('Page 2')
-elif choice == 'Page3':
-    st.title('Page 3')
+        @st.cache
+        def load_data(nrows):
+            data = pd.read_csv(DATA_URL, nrows=nrows)
+            lowercase = lambda x: str(x).lower()
+            data.rename(lowercase, axis='columns', inplace=True)
+            data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+            return data
 
-next = st.button('Go to next page')
+        data_load_state = st.text('Loading data...')
+        data = load_data(10000)
+        data_load_state.text("Done! (using st.cache)")
 
+        if st.checkbox('Show raw data'):
+            st.subheader('Raw data')
+            st.write(data)
+
+        st.subheader('Number of pickups by hour')
+        hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+        st.bar_chart(hist_values)
+
+        # Some number in the range 0-23
+        hour_to_filter = st.slider('hour', 0, 23, 17)
+        filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+
+        st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+        st.map(filtered_data)
+        #-------------------existing untouched code------------------------------------------
