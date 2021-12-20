a4d7426a3601877ba6524c781b2895ddda1623ae
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 15:29:43 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ st.markdown("""
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
-        <a class="nav-link disabled" href="#">                                                      <span class="sr-only">(current)</span></a>
+        <a class="nav-link disabled" href="#"> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item active">
         <a class="nav-link disabled" href="#">Home <span class="sr-only">(current)</span></a>
