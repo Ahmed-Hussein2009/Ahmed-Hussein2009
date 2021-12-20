0150778d3fde4dd8e02a81c6a7953d1ee1d48d3b
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 12:37:33 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,5 +1,5 @@
+app = HydraApp(title='Secure Hydralit Data Explorer',favicon="?",hide_streamlit_markers=True,use_navbar=True, navbar_sticky=True)
 
-import streamlit as st
 """
 st.title("Home")
 st.image("Home1.png",width=900)  
@@ -13,49 +13,3 @@ if st.button('Say hello'):
 
 st.image("footer.png",width=900)   
 """
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-#add an import to Hydralit
-from hydralit import HydraHeadApp
-
-#create a wrapper class
-class MySampleApp(HydraHeadApp):
-
-#wrap all your code in this method and you should be done
-    def run(self):
-        #-------------------existing untouched code------------------------------------------
-        st.title('Uber pickups in NYC')
-
-        DATE_COLUMN = 'date/time'
-        DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-                    'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
-
-        @st.cache
-        def load_data(nrows):
-            data = pd.read_csv(DATA_URL, nrows=nrows)
-            lowercase = lambda x: str(x).lower()
-            data.rename(lowercase, axis='columns', inplace=True)
-            data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-            return data
-
-        data_load_state = st.text('Loading data...')
-        data = load_data(10000)
-        data_load_state.text("Done! (using st.cache)")
-
-        if st.checkbox('Show raw data'):
-            st.subheader('Raw data')
-            st.write(data)
-
-        st.subheader('Number of pickups by hour')
-        hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-        st.bar_chart(hist_values)
-
-        # Some number in the range 0-23
-        hour_to_filter = st.slider('hour', 0, 23, 17)
-        filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-        st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-        st.map(filtered_data)
-        #-------------------existing untouched code------------------------------------------
