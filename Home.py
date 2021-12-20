daade57bd8347e6fb3ff925520965ceb6a0a3222
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 09:47:09 +0200

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+
+st.title("About us")
+
+
+
+st.image("about.png")
+#width=650   
+         
+         
+         
+         
+         
+        
