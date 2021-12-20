cae6c5385167eee0a4566a8f7bd467f31682d540
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 11:51:46 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ img1="Home2.png"
 img1="Home3.png"
 start_color, end_color = st.select_slider(
      'Gallary',
-     Option=['','','']
+     Option=['','',''],
      value=st.image("Home2.png",width=900))
 st.write(img1)         
 img4=st.image("Home4.png",width=900)      
