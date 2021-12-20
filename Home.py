fff1e529804952d428ab753041bfddc10c08ffea
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 12:01:32 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,15 +1,8 @@
 import streamlit as st
 st.title("Home")
 st.image("Home1.png",width=900)  
-
-img1="Home1.png"
-img1="Home2.png"
-img1="Home3.png"
-start_color, end_color = st.select_slider(
-     'Gallary',
-     Option=['','',''],
-     value=st.image("Home2.png",width=900))
-st.write(img1)         
+st.image("Home2.png",width=900)  
+st.image("Home3.png",width=900)  
 img4=st.image("Home4.png",width=900)      
          
          
