7f9c3dd11e276226be340bb1e5d27a91f8d4271a
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 20:39:01 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -201,7 +201,7 @@ iframe[sandbox] .read-article{
     <div class="slides">
       <div id="slides__1" class="slide">
         <span class="slide__text">1</span>
-        <a class="slide__prev" href="#slides__4" title="Next"></a>
+        <a class="slide__prev" href="#slides__3" title="Next"></a>
         <a class="slide__next" href="#slides__2" title="Next"></a>
       </div>
       <div id="slides__2" class="slide">
@@ -212,12 +212,7 @@ iframe[sandbox] .read-article{
       <div id="slides__3" class="slide">
         <span class="slide__text">3</span>
         <a class="slide__prev" href="#slides__2" title="Prev"></a>
-        <a class="slide__next" href="#slides__4" title="Next"></a>
-      </div>
-      <div id="slides__4" class="slide">
-        <span class="slide__text">4</span>
-        <a class="slide__prev" href="#slides__3" title="Prev"></a>
-        <a class="slide__next" href="#slides__1" title="Prev"></a>
+        <a class="slide__next" href="#slides__1" title="Next"></a>
       </div>
     </div>
     <div class="slider__nav">
