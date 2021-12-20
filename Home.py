fe5dc09da6cce546dc2efb6370a9d2b4de25b302
Ahmed-Hussein2009 <33674887+Ahmed-Hussein2009@users.noreply.github.com>
2021-12-20 13:50:30 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,6 +4,7 @@ st.image("Home1.png",width=900)
 st.image("Home2.png",width=900)  
 st.image("Home3.png",width=900)  
 img4=st.image("Home4.png",width=900)  
+st.button("Click me for no reason")
 if st.button('Say hello'):
      st.write('Why hello there')
  else:
